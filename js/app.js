ce047a463bc61e5e95cd0789062c6cb55b8d2e44
Stella-Chaves <stellachavesyoga@gmail.com>
2018-10-17 08:00:32 -0500

    map var

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ var model = {
 
 }
 // Veiw
+var map;
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 47.6199455, lng: -122.3512352},
+    zoom: 13
+  });
+}
+
 var mapView = {
 
 }

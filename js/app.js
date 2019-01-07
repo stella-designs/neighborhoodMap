65dc335b393cb0cf3fb8f2fa11bfd18842a3f8a2
Stella-Ray-Chaves <stellachavesyoga@gmail.com>
2019-01-07 14:15:22 -0600

    locations

--- js/app.js
+++ js/app.js
@@ -2,7 +2,20 @@
 var model = {
 
 }
+
+var locations = [
+  {title: 'Pike Place Market', location: {lat: 47.610136, lng: -122.342057}},
+  {title: 'Washington Park Arboretum', location: {lat: 47.630101, lng: -122.294548}},
+  {title: 'Simply Soulful', location: {lat: 47.624056, lng: -122.294928}},
+  {title: 'Bacco Cafe', location: {lat: 47.610097, lng: -122.341406}},
+  {title: 'Seaward Park', location: {lat: 47.549605, lng: -122.257357}},
+  {title: 'Mashiko', location: {lat: 47.560369, lng: -122.386989}},
+]
+
 // Veiw
+var mapView = {
+
+}
 var map;
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
@@ -12,9 +25,6 @@ function initMap() {
   });
 }
 
-var mapView = {
-
-}
 // Controller
 var controller = {
     

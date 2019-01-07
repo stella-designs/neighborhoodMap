285d50aa5d10f76d6fad9b0af3e2297a511c0c11
Stella-Ray-Chaves <stellachavesyoga@gmail.com>
2019-01-07 15:01:35 -0600

    infowindow

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-// Model
-var model = {
+// Model (application data objects are stored)
+var model = {}
 
+var map;
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 47.6199455, lng: -122.3512352},
+    zoom: 13
+  });
 }
 
 var locations = [
@@ -12,20 +19,46 @@ var locations = [
   {title: 'Mashiko', location: {lat: 47.560369, lng: -122.386989}},
 ]
 
-// Veiw
-var mapView = {
+// Veiw (whats presented to the users)
+var mapView = {}
 
-}
-var map;
-function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.6199455, lng: -122.3512352},
-    zoom: 13
+var largeInfowindow = new google.maps.InfoWindow();
+
+//Location array to create an array of markers on initalize.
+for (var i =0; i < locations.length; i++) {
+  // Get the position from the location array
+  var position = location[i].locations;
+  var title = locations[i].title;
+  //Create a marker per location, and put into markers array
+  var marker = new.google.maps.Marker({
+    position: position,
+    title: title,
+    animation: google.maps.Animation.DROP,
+    id: i
+  });
+  // Push the marker to array of markers
+  markers.push(marker);
+  // create an onclick event to open an infowindow at each marker
+  marker.addListner('click', function() {
+    populateInfoWindow(this, largeInfowindow);
   });
 }
+document.getElementById('show-listings').addEventListener('click', showListings);
+document.getElementById('hide-listings').addEventListener('click', hideListings);
 
-// Controller
-var controller = {
-    
+// Populates infowindow when marker is clicked, allowing only one to open, populate based on location
+function populateInfoWindow(marker, infowindow) {
+  //Check to make sure info window is not already open
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '<div>');
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if infowindow is closed
+    infowindow.addListner('closeclick', function() {
+      infowindow.marker = null;
+    });
+  }
 }
+
+// Controller (decision maker and glue between model and view)
+var controller = {}
